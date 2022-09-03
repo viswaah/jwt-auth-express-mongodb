42837d90f47d781f1fe722f8b6ece553374d250e
vishwaah <vishwajenvish@gmail.com>
2022-09-03 16:15:30 +0530

    add new fields to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,14 +2,20 @@ import { Schema, model } from "mongoose";
 
 interface IUser {
   email: string;
-  name: string;
+  userId: string;
   password: string;
+  emailToken: string;
+  emailTokenExp: Date;
+  activated: boolean;
 }
 
 const userSchema = new Schema<IUser>({
-  email: { type: String, required: true },
-  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  userId: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  activated: { type: Boolean, default: false },
+  emailToken: { type: String, default: null },
+  emailTokenExp: { type: Date, default: null },
 });
 
 export default model<IUser>("User", userSchema);
